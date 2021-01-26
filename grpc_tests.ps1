0bce6999b3aafc4934a6bac5631f14e975199ad3
= <dompace181@gmail.com>
2021-01-26 12:00:42 -0600

    tests stuff

--- grpc_tests.ps1
+++ grpc_tests.ps1
@@ -1,3 +1,6 @@
 #grpcurl -v -plaintext -d '{\"phone\": \"8479095558\", \"fname\":\"d\", \"lname\":\"p\" }' localhost:9000 CPUser.CPUser/CreateUser
-grpcurl -v -plaintext -d '{ \"authRequest\": { \"Token\": \"W3rci8O1I6LvBoBcAhEwQzTv\" }, \"tableToken\": \"1234\" }' checkplease.app:9000 CPUser.CPUser/OrderInitiation
-grpcurl -v -plaintext -d '{ \"authRequest\": { \"Token\": \"W3rci8O1I6LvBoBcAhEwQzTv\" }, \"tokenCode\": \"1234\" }' checkplease.app:9000 CPUser.CPUser/SelectionSubscribe
+#grpcurl -v -plaintext -d '{ \"authrequest\": { \"token\": \"w3rci8o1i6lvbobcahewqztv\" }, \"tabletoken\": \"1234\" }' checkplease.app:9000 cpuser.cpuser/orderinitiation
+#grpcurl -v -plaintext -d '{ \"authrequest\": { \"token\": \"w3rci8o1i6lvbobcahewqztv\" }, \"tokencode\": \"1234\" }' checkplease.app:9000 cpuser.cpuser/ItemPaySubscribe
+
+grpcurl -v -plaintext -d '{ \"authRequest\": { \"Token\": \"EpLj3ssCAIsuaVMJJinsSVBY\" }, \"tableToken\": \"12345\" }' localhost:9000 CPUser.CPUser/OrderInitiation
+grpcurl -v -plaintext -d '{ \"authRequest\": { \"Token\": \"EpLj3ssCAIsuaVMJJinsSVBY\" }, \"tokenCode\": \"12345\" }' localhost:9000 CPUser.CPUser/ItemPaySubscribe
